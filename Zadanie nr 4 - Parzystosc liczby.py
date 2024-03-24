5f770ee9868c9a206ccbca7f6d9d2de695433d4a
LukaszChodzicki <164647026+LukaszChodzicki@users.noreply.github.com>
2024-03-24 12:59:07 +0100

    Add files via upload

---
+++ Zadanie nr 4 - Parzystosc liczby.py
@@ -0,0 +1,7 @@
+print("Witamy w programie - Parzystość liczby")
+print("Podaj liczbę całkowitą")
+a = int(input())
+if a % 2 == 0:
+    print("Wprowadzona liczba", a, "jest parzysta")
+else:
+    print("Wprowadzona liczba", a, "jest nieparzysta")
